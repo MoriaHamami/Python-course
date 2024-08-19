1692e5486fb624651ad02130cc57e5936ecbab37
Moria Hamami <moriahamami@gmail.com>
2024-08-19 22:47:33 +0300

    Solved A-4 (grade 100)

--- Assignments/A-4/allSums.py
+++ Assignments/A-4/allSums.py
@@ -1,3 +1,12 @@
 def allSumsDP(arr):
- 
-    pass
+    sums = set()
+    # Not choosing any number results in sum zero
+    sums.add(0)
+    # Go over each number in array
+    for num in arr:
+        new_sums = set()
+        # Add the number to each sum ("set" doesn't save duplicates)
+        for sum in sums:
+            new_sums.add(sum + num)
+        sums.update(new_sums)
+    return sums
